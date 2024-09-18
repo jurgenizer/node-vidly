b8e127a175ebaaf0b95148c7d9e0c607a7106453
Jurgen <jurgen.geitner@gmail.com>
2024-09-18 11:05:26 +0200

    POST request works

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ app.get('/api/genres', (req, res) => {
 });
 
 
-// Create a video genres
+// POST a new genre (create a new genre and return the genre object)
+app.post('/api/genres', (req, res) => {
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+    genres.push(genre);
+    res.send(genre);
+})
+
 
 
 
