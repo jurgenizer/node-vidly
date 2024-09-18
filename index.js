b9c7ccaa058158d333ff153c14da9caebf4c2064
Jurgen <jurgen.geitner@gmail.com>
2024-09-18 10:52:26 +0200

    simple api for video genres with get request

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const Joi = require('joi');
+const express  = require('express');
+const app = express();
+app.use(express.json());
+
+// http://vidly.comn/api/genres
+
+const genres = [
+    { id: 1, name: 'comedy' },
+    { id: 2, name: 'romance' },
+    { id: 3, name: 'thriller' },
+    { id: 4, name: 'action' },
+    { id: 5, name: 'documentary' },
+    { id: 6, name: 'animation' },
+    { id: 7, name: 'drama' },
+    { id: 8, name: 'science fiction' },
+    { id: 9, name: 'fantasy' },
+    { id: 10, name: 'martial arts' },
+    { id: 11, name: 'mystery' },
+    { id: 12, name: 'musical' }
+]
+// GET all the video genres
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
+
+// Create a video genres
+
+
+
+//Read port Variable from environment or default to 3000
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on Port ${port}...`));
