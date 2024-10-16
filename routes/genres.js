0c58eb6d7faf3f25f6fa160807537d7648296bbf
Jurgen Geitner <jurgen.geitner@gmail.com>
2024-10-16 10:02:32 +0200

    add persisitance to vidly genres API

--- routes/genres.js
+++ routes/genres.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Joi = require('joi');
 
 // Schema
-const genreSchema = new Mongoose.Schema({
+const genreSchema =  mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -15,10 +15,10 @@ const genreSchema = new Mongoose.Schema({
 });
 
 // Model
-const Genre = new mongoose.model('Genre', genreSchema);
+const Genre = mongoose.model('Genre', genreSchema);
 
 
-const genres = [
+/* const genres = [
     { id: 1, name: 'comedy' },
     { id: 2, name: 'romance' },
     { id: 3, name: 'thriller' },
@@ -31,67 +31,56 @@ const genres = [
     { id: 10, name: 'martial arts' },
     { id: 11, name: 'mystery' },
     { id: 12, name: 'musical' }
-]
+] */
 
 
 // GET all the video genres
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const genres = await Genre.find().sort('name');
     res.send(genres);
 });
 
 
 // GET a single video genre
-router.get('/:id', (req, res) => {
-    const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+router.get('/:id', async (req, res) => {
+    const genre = await Genre.findById(req.params.id)
+
     if (!genre) return res.status(404).send('The video genre with the requested ID was not found');
     //console.log(genre);
     res.send(genre);
 })
 
 // POST a new genre (create a new genre and return the genre object)
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     // validateGenre() with object destructuring to get error property
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    }
-    genres.push(genre);
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
     res.send(genre);
-})
+});
 
 // PUT a single video genre (update the genre)
-router.put('/:id', (req, res) => {
-    // Lookup the genre
-    const genre = genres.find(genre => genre.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('The video genre with the requested ID was not found');
-
+router.put('/:id', async (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    if (error) {
-        // 400 bad request
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-    // Update the genre
-    genre.name = req.body.name;
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+        new: true
+    });
+
+    if (!genre) return res.status(404).send('The video genre with the requested ID was not found');
 
     //Return the updated genre
     res.send(genre);
 })
 
 // DELETE a single video genre 
-router.delete('/:id', (req, res) => {
-    // Lookup the genre
-    const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+router.delete('/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndDelete(req.params.id);
+
     // console.log('The genre to be deleted is ', genre);
     if (!genre) return res.status(404).send('The video genre with the requested ID was not found');
-
-    // Delete 
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
 
     // Return deleted genre
     res.send(genre);
