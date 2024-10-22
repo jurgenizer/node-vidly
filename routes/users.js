7e6a6f8dd5650702c1a2bab9319a483a48e81835
Jurgen Geitner <jurgen.geitner@gmail.com>
2024-10-22 15:26:18 +0200

    hashing and salting passwords

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 // http://vidly.comn/api/users
+const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
@@ -9,9 +10,9 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-   let user = await User.findOne({email: req.body.email})
-    if(user) return res.status(400).send('User aleady registered.');
-  
+    let user = await User.findOne({ email: req.body.email })
+    if (user) return res.status(400).send('User aleady registered.');
+
     //reset the user object
 
     /* user = new User({
@@ -19,9 +20,11 @@ router.post('/', async (req, res) => {
         email: req.body.email,
         password: req.body.password
     }) */
-// or using lodash
+    // or using lodash
 
-        user = new User(_.pick(req.body, ['name', 'email', 'password']))
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
     await user.save();
 
