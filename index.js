60345a9ecfdb2de8f85894d385d3d7cbca5c1326
Jurgen Geitner <jurgen.geitner@gmail.com>
2024-10-22 17:49:59 +0200

    authenticate a user

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const debug = require('debug')('app:startup');
 const config = require("dotenv").config();
 const morgan = require('morgan');
 const helmet = require('helmet');
-
 const logger = require('./middleware/logger');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -31,6 +31,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 
 // Middleware 
